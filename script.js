f41e8ad5fb059d3cbe3f530d92ab7214be54e2d4
Itai <ezbloy@gmail.com>
2020-08-28 18:21:50 +0300

    commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 
-
 let matrix = [
     [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5],
     [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5],
@@ -74,24 +73,30 @@ function setWorld(){
 setWorld();
 
 let hand = "";
+let pickaxe = document.querySelector(".pickaxe-arsenal");
+let axe = document.querySelector(".axe-arsenal");
+let shoval = document.querySelector(".shoval-arsenal");
+let reset = document.querySelector(".reset");
 
-world.forEach(item => {
-    item.addEventListener('click', event => {
-          alert("BOOM!!!");
-    })
-  })
+// world.forEach(item => {
+//     item.addEventListener('click', event => {
+//           alert("BOOM!!!");
+//     })
+//   })
 
-document.querySelector(".pickaxe-arsenal").addEventListener("click", function(){
+pickaxe.addEventListener("click", function(){
+    console.log(hand);
     if (hand === "pickaxe-arsenal"){hand = ""; this.style.background = "none";} 
     else{
         document.querySelector(".shoval-arsenal").style.background = "none";
         document.querySelector(".axe-arsenal").style.background = "none";  
-        hand = "pickaxe-arsenal"; 
+        hand = "pickaxe-arsenal";
+        console.log(hand);
         this.style.background = "blue";
     }
     
 });
-document.querySelector(".shoval-arsenal").addEventListener("click", function(){
+shoval.addEventListener("click", function(){
     if (hand === "shoval-arsenal"){hand = ""; this.style.background = "none";} 
     else{
         document.querySelector(".pickaxe-arsenal").style.background = "none";
@@ -99,7 +104,7 @@ document.querySelector(".shoval-arsenal").addEventListener("click", function(){
         hand = "shoval-arsenal"; 
         this.style.background = "blue";}
 });
-document.querySelector(".axe-arsenal").addEventListener("click", function(){
+axe.addEventListener("click", function(){
     if (hand === "axe-arsenal"){hand = ""; this.style.background = "none";} 
     else{
         document.querySelector(".shoval-arsenal").style.background = "none";
@@ -108,9 +113,13 @@ document.querySelector(".axe-arsenal").addEventListener("click", function(){
         this.style.background = "blue";}
 });
 
-// document.querySelector(".reset").addEventListener("click", function(){
-//     for (let i = 0; i < 400; i++){
-//         document.querySelector(".world").deleteCell(0)
-//     }
-//     setWorld()
-// });
+reset.addEventListener("click", function(){
+    for (let i = 0; i < 400; i++){
+        document.querySelector(".world").deleteCell(0)
+    }
+    setWorld()
+});
+
+if (hand !== ""){
+    document.style.cursur = "pointer";
+}
